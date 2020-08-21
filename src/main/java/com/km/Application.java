c830559d46891fcbc57a4b4f86194b16ac0607ce
Kacper Marczewski <kacper.marczewski@gmail.com>
2020-08-21 23:24:51 +0200

    initial commit

--- src/main/java/com/km/Application.java
+++ src/main/java/com/km/Application.java
@@ -1,7 +1,25 @@
 package com.km;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class Application {
+    private static final String CONF_FILE = "editable/conf";
+    private static final String VERSION = "version";
+
     public static void main(String... args) {
-        System.out.println("Hello !!!");
+        Properties properties = loadConfig();
+        System.out.printf("latest version is %s", properties.get(VERSION));
+    }
+
+    private static Properties loadConfig() {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream(CONF_FILE));
+        } catch (Exception e) {
+            System.out.printf("Error reading config from [%s]%n", CONF_FILE);
+            System.exit(0);
+        }
+        return properties;
     }
 }
